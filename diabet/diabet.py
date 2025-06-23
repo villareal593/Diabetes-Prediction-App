3c879685b28ce290ee823205efbbd7d9be7bdb60
villareal593 <niwaldy@gmail.com>
2025-06-23 08:53:35 +0300

    Update diabet.py

--- diabet/diabet.py
+++ diabet/diabet.py
@@ -1,4 +1,4 @@
-#Importing the required modules and libraries
+#Импорт необходимых модулей и библиотек
 import pandas as pd
 
 import numpy as np
@@ -10,7 +10,7 @@ import warnings
 import pickle
 warnings.filterwarnings('ignore')
 
-#Dataframe
+#Данные
 df = pd.read_csv('diabetes.csv')
 
 features=['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin',
@@ -18,19 +18,19 @@ features=['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin',
 X=pd.get_dummies(df[features])
 y=df.Outcome
 
-#Scaling the datas using standar scaler
+#Масштабирование данных
 scaler = StandardScaler()
 standardized_data = scaler.fit_transform(X)
 X=standardized_data
 
-#splitting the datas for training and testing
+#разделение данных для обучения и тестирования
 x_train,x_test,y_train,y_test=train_test_split(X,y,test_size=1/11,random_state=242)
 
-#Model
+#модель
 lda = LinearDiscriminantAnalysis()
 lda.fit(x_train,y_train)
 X_train_prediction = lda.predict(x_test)
 
-#Linking with streamlit for web application
+#Связывание с streamlit для веб-приложения
 pickle.dump(lda,open('model.pkl','wb'))
 pickle.dump(scaler, open('scaler.pkl', 'wb'))
