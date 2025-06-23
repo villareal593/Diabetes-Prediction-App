3118d86c87d8384d217cff612bcd3accb9998a54
villareal593 <niwaldy@gmail.com>
2025-06-23 08:55:03 +0300

    Update web_diabet.py

--- diabet/web_diabet/web_diabet.py
+++ diabet/web_diabet/web_diabet.py
@@ -1,36 +1,36 @@
-#Importing Libraries and Modules
+#Импорт библиотек и модулей
 import streamlit as st
 import pickle
 import numpy as np
 
-#Loading Model and scaling files
+#Загрузка файлов модели и масштабирования
 model=pickle.load(open('model.pkl','rb'))
 scaler=pickle.load(open('scaler.pkl','rb'))
 
-#Scaling the input
+#Масштабирование входных данных
 def preprocess_input(user_input, scaler):
     user_input_np = np.array(user_input).reshape(1, -1)
     scaled_input = scaler.transform(user_input_np)
     return scaled_input
 
-#prediction
+#прогноз
 def predict_diabetes(user_input):
     scaled_input = preprocess_input(user_input, scaler)
     prediction = model.predict(scaled_input)
     return prediction
 
-#Web Application
+#веб-приложение
 st.markdown("""
     <h1 style='text-align: center; color: #ff5733;'>Diabetes Prediction App</h1>
     <p style='text-align: center;'>Enter the required information to predict whether diabetes is present or not.</p>
 """, unsafe_allow_html=True)
 
-#User Input from Web Application
+#Пользовательский ввод из веб-приложения
 user_input = []
 for feature in ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin',
                     'BMI', 'DiabetesPedigreeFunction', 'Age']:user_input.append(st.number_input(f'Enter {feature}', step=0.01))
 
-#Prediction
+#кнопка
 if st.button('Predict'):
     prediction = predict_diabetes(user_input)
     if prediction[0] == 0:
